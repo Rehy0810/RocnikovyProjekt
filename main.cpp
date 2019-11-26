56d50e69b7d9e576febedaa6aab49211c36e38c7
Rehy0810 <dragokuba@email.cz>
2019-11-26 11:05:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,15 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 
+const int stepsPerRevolution = 200;
+Stepper myStepper(stepsPerRevolution, 5, 4, 2, 14);
 
-//const char *ssid     = "ESPNet";
-//const char *password = "";
 
+bool probiha=false;
+
+char hodiny[4]={
+ 11, 12, 15, 18 
+  };
 const long utcOffsetInSeconds = 3600;
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
@@ -18,24 +23,18 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "192.168.1.1", utcOffsetInSeconds);
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(9600);
    WiFiManager wifiManager;
   wifiManager.autoConnect("ArduinoESP.");
-
-  //if you get here you have connected to the WiFi
   Serial.println("connected...");
-  /*WiFi.begin(ssid, password);
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 ); 
-    Serial.print ( "." );
-  }*/
-
   timeClient.begin();
+  myStepper.setSpeed(60);
 }
 
 void loop() {
-  timeClient.update();
-
+  
+  if(millis()%100 == 0){
+    timeClient.update();
   Serial.print(daysOfTheWeek[timeClient.getDay()]);
   Serial.print(", ");
   Serial.print(timeClient.getHours());
@@ -43,5 +42,21 @@ void loop() {
   Serial.print(timeClient.getMinutes());
   Serial.print(":");
   Serial.println(timeClient.getSeconds());
-  delay(1000);
+  }
+  for(int i=0;i<3;i++){
+  if((timeClient.getHours()==hodiny[i] && timeClient.getMinutes() == 03 && timeClient.getSeconds() <= 50) != probiha){
+    Serial.print("Rehy");
+    otoceni();
+  }
+    
+  }
+}
+
+void otoceni(){
+ probiha=true;
+ Serial.print("sdfha");
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+ probiha=false;
+
 }
